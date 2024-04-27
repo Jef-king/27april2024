fd3ef2008bb4c8e3c1fc0fc6e6d000a4c49bf2b1
jefking <jefking@outlook.in>
2024-04-27 19:25:11 +0530

    check

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 let erpas=document.querySelector('.erpassword');
+let eremail=document.querySelector('.eremail');
+let email=document.querySelector('.email');
+let password=document.querySelector('.password');
+let success=documnet.querySelector('.success');
+email.addEventListener('blur',()=>{
+    let validate=/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    let emailadd=email.ariaValueMax;
+    if(!validate.test(emailadd)){
+        
+    }
+})
